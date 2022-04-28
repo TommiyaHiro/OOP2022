13db5f1c80666687d7a84c8bbcc227175cc489a9
陽色 頓宮 <32234@ojs.ac.jp>
2022-04-28 15:05:36 +0900

    メソッドの移動

--- Chapter02/SalecesCalculator/SalesCounter.cs
+++ Chapter02/SalecesCalculator/SalesCounter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,5 +27,23 @@ namespace SalecesCalculator {
             }
             return dict;
         }
+
+        // 売上データを読み込み、Saleオブジェクトのリストを返す
+        public static List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach(string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+
+            return sales;
+        }
+
     }
 }
