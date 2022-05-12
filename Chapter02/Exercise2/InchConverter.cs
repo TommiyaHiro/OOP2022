659f77e34e84dbee91d6d2546c72089371697be2
陽色 頓宮 <32234@ojs.ac.jp>
2022-05-12 13:58:28 +0900

    問題２－２完成

---
+++ Chapter02/Exercise2/InchConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise2{
+
+    // インチとメートルの単位変換クラス
+    public static class InchConverter {
+        private const double ratio = 0.0254;
+
+        // インチからフィートを求める
+        public static double MeterToInch(double meter) {
+            return meter / ratio;
+        }
+
+        // インチからメートルを求める
+        public static double InchToMeter(double inch) {
+            return inch * ratio;
+        }
+    }
+}
---
+++ Chapter02/Exercise2/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace Excercise2 {
+    class Program {
+        //コマンドライン引数
+        static void Main(string[] args) {
+            if(args.Length >= 1 && args[0] == "-tof") {
+                // インチからメートルへの対応表を出力
+                PrintInchToMeterList(1, 10);
+            }
+            else {
+                // メートルからインチへの対応表を出力
+                PrintMeterToInchList(1, 10);
+            }
+        }
+        //インチからメートルへの対応表を出力
+        private static void PrintInchToMeterList(int start, int stop) {
+            //FeetConverter fc = new FeetConverter();
+            for(int inch = start; inch <= stop; inch++) {
+                double meter = InchConverter.InchToMeter(inch);
+                Console.WriteLine("{0} inch = {1:0.0000} m", inch, meter);
+            }
+        }
+        //メートルからインチへの対応表を出力
+        private static void PrintMeterToInchList(int start, int stop) {
+            //FeetConverter fc = new FeetConverter();
+            for(int meter = 1; meter <= 10; meter++) {
+                double inch = InchConverter.MeterToInch(meter);
+                Console.WriteLine("{0} m = {1:0.0000} inch", meter, inch);
+            }
+        }
+    }
+}
