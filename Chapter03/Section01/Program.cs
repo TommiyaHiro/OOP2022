8f82c5fb7aeb065997918a62434a0f9a63e145fd
陽色 頓宮 <32234@ojs.ac.jp>
2022-05-13 15:16:54 +0900

    ラムダ演算子

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        //public delegate bool Judgement(int value);    // デリゲートの宣言
+
+        //static void Main(string[] args) {
+        //    var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+
+        //    int count = Count(numbers, n => n.ToString().Contains('1'));  //n => n % 2 == 0  ラムダ式を使った処理
+
+        //    Console.WriteLine( "個数：" + count);
+        //}
+
+        //public static int Count(int[] numbers,Predicate<int> judge) {
+        //    int count = 0;
+        //    foreach(var n in numbers) {
+        //        if(judge(n) == true) {
+        //            count++;
+        //        }
+        //    }
+        //    return count;
+        //}
+
+        static void Main(string[] args) {
+            var list = new List<string> {
+               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            var names = list.FindAll(s => s.Length <= 5);
+            names.ForEach(s => Console.WriteLine(s));
+
+            //foreach(var s in names) {
+            //    Console.WriteLine(s);
+        }
+
+        }
+    }
+
