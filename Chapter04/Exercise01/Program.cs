8e104d6df85defca3b4cc45d46116fe3ac17f982
陽色 頓宮 <32234@ojs.ac.jp>
2022-05-20 15:55:43 +0900

    問題４－１完成

---
+++ Chapter04/Exercise01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            var ym = new YearMonth(2004, 12);
+
+            var check = ym.Is21Century;
+            var test = ym.AddOneMonth();
+
+            Console.WriteLine(ym);
+        }
+    }
+}
---
+++ Chapter04/Exercise01/YearMonth.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class YearMonth {
+        // ４－１－１
+        public int Year { get; private set; }
+
+        public int Month { get; private set; }
+
+        public YearMonth(int year, int month) {
+            Year = year;
+            Month = month;
+        }
+
+        // ４－１－２
+        public bool Is21Century {
+            get {
+                return 2001 <= Year && Year <= 2100;
+            }
+        }
+
+        // ４－１－３
+        public YearMonth AddOneMonth() {
+            if(Month == 12)
+                return new YearMonth(this.Year + 1, 1);
+            else
+                return new YearMonth(this.Year, this.Month + 1);
+
+        }
+
+        // ４－１－４
+        public override string ToString() {
+            return $"{Year}年{Month}月";
+        }
+    }
+}
