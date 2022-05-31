f54c2126bb8094b6317d5568c82f22f810f009cc
陽色 頓宮 <32234@ojs.ac.jp>
2022-05-31 14:51:19 +0900

    第5章まで完成

--- Chapter05/Exercise04/Program.cs
+++ Chapter05/Exercise04/Program.cs
@@ -8,8 +8,10 @@ namespace Exercise04 {
     class Program {
         static void Main(string[] args) {
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
-
-
+            foreach(var item in line.Split(';')) {
+                var array = item.Split('=');
+                Console.WriteLine("{0}:{1}", ToJapanese(array[0]), array[1]);
+            }
         }
 
         static string ToJapanese(string key) {
