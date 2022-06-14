511c58c80b4f08a8797a79f4c03c5b05a91bbc68
陽色 頓宮 <32234@ojs.ac.jp>
2022-06-14 14:29:40 +0900

    途中

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -7,16 +7,23 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
-
-
-
-            
+            _score = ReadScore(filePath);
         }
 
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
+            List<Student> students = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach(string line in lines) {
+                string[] items = line.Split(',');
+                Student student = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                students.Add(student);
+            }
 
 
 
--- Test202206/TestProject/Test01/Student.cs
+++ Test202206/TestProject/Test01/Student.cs
@@ -1,5 +1,13 @@
 ﻿namespace Test01 {
-    class Student {
+    public class Student {
+        //学生の名前
+        public string Name { get; set; }
+
+        //科目名
+        public string Subject { get; set; }
+
+        // 点数
+        public int Score { get; set; }
 
 
 
