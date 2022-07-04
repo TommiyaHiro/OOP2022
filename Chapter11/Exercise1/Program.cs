5227b9d4f4bb9afff7020ec4e92627dfcde4d1b7
陽色 頓宮 <32234@ojs.ac.jp>
2022-07-04 11:43:45 +0900

    11-1-1完成

---
+++ Chapter11/Exercise1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+            var file = "sample.xml";
+            Exercise1_1(file);
+            Console.WriteLine();
+            Exercise1_2(file);
+            Console.WriteLine();
+            Exercise1_3(file);
+            Console.WriteLine();
+
+            var newfile = "sports.xml";
+            Exercise1_4(file, newfile);
+        }
+
+        private static void Exercise1_1(string file) {
+            var xdoc = XDocument.Load("Sample.xml");
+            var sports = xdoc.Root.Elements().Select(x => new {
+                SportsName = (string)x.Element("name"),
+                TeamMember = (int)x.Element("teammembers")
+
+            });
+            foreach(var Sports in sports) {
+                Console.WriteLine("{0} {1} ", Sports.SportsName, Sports.TeamMember);
+
+            }
+        }
+
+        private static void Exercise1_2(string file) {
+        }
+
+        private static void Exercise1_3(string file) {
+        }
+
+        private static void Exercise1_4(string file, string newfile) {
+        }
+    }
+}
--- Chapter11/XmlSample/Program.cs
+++ Chapter11/XmlSample/Program.cs
@@ -12,18 +12,19 @@ namespace XmlSample
     {
         static void Main(string[] args)
         {
-            using(var wc = new WebClient()) {
-                var stream = wc.OpenRead("https://news.yahoo.co.jp/rss/media/jomo/all.xml");
-
-                var xdoc = XDocument.Load(stream);
-                var xNews = xdoc.Root.Descendants("item").Select(x=>(string)x.Element("title"));
-
-                foreach(var data in xNews) {
-                    Console.WriteLine(data);
-
-                }
-
+            var xdoc = XDocument.Load("novelists.xml");
+            var novelists = xdoc.Root.Elements()
+                                .Select(x => new {
+                                    Name = (string)x.Element("name"),
+                                    Birth = (DateTime)x.Element("birth"),
+                                    Death = (DateTime)x.Element("death")
+                                });
+            foreach(var novelist in novelists) {
+                Console.WriteLine("{0} ({1}-{2})",
+                                   novelist.Name, novelist.Birth.Year, novelist.Death.Year);
             }
         }
     }
 }
+
+
