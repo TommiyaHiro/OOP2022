5ed2918bb3431628fbcfb02788c28230a0d5cd58
陽色 頓宮 <32234@ojs.ac.jp>
2022-10-06 15:53:54 +0900

    １５章演習１－６途中

--- Chapter15/Exercise1/Program.cs
+++ Chapter15/Exercise1/Program.cs
@@ -33,8 +33,15 @@ namespace Exercise1 {
         }
 
         private static void Exercise1_3() {
-            //var groups = Library.Books
-                
+            var groups = Library.Books
+                .GroupBy(b => b.PublishedYear)
+                .Select(g => new {
+                    Year = g.Key,
+                    Count = g.Count()
+                });
+            foreach(var g in groups) {
+                Console.WriteLine($"{g.Year} {g.Count}冊");
+            }
         }
 
         private static void Exercise1_4() {
@@ -53,7 +60,7 @@ namespace Exercise1 {
                 .OrderByDescending(x => x.PublishedYear)
                 .ThenByDescending(x => x.Price)) {
 
-                Console.WriteLine($"{book.PublishedYear}年,{book.Price}円,{book.Title},({book.Category})");
+                Console.WriteLine($"{book.PublishedYear}年 {book.Price}円 {book.Title} ({book.Category})");
             }
         }
 
@@ -71,9 +78,24 @@ namespace Exercise1 {
         }
 
         private static void Exercise1_6() {
+            var groups = Library.Categories
+                .GroupJoin(Library.Books,
+                c => c.Id,
+                b => b.CategoryId,
+                (c, books) => new { Category = c.Name, Books = books })
+                .OrderBy(x => x.Category);
+            foreach(var group in groups) {
+                Console.WriteLine($"#{group.Category}");
+                foreach(var book in group.Books) {
+                    Console.WriteLine($" {book.Title}");
+                }
+
+            }
+            
         }
 
         private static void Exercise1_7() {
+
         }
 
         private static void Exercise1_8() {
