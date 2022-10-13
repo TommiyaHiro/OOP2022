546c789ecb8b895ac5053423a8f9c5d3d7074f96
陽色 頓宮 <32234@ojs.ac.jp>
2022-10-13 11:09:55 +0900

    chapter15

--- Chapter15/Exercise1/Program.cs
+++ Chapter15/Exercise1/Program.cs
@@ -78,27 +78,69 @@ namespace Exercise1 {
         }
 
         private static void Exercise1_6() {
-            var groups = Library.Categories
-                .GroupJoin(Library.Books,
-                c => c.Id,
-                b => b.CategoryId,
-                (c, books) => new { Category = c.Name, Books = books })
-                .OrderBy(x => x.Category);
-            foreach(var group in groups) {
-                Console.WriteLine($"#{group.Category}");
-                foreach(var book in group.Books) {
-                    Console.WriteLine($" {book.Title}");
-                }
+            //var groups = Library.Categories
+            //    .GroupJoin(Library.Books,
+            //    c => c.Id,
+            //    b => b.CategoryId,
+            //    (c, books) => new { Category = c.Name, Books = books })
+            //    .OrderBy(x => x.Category);
+            //foreach(var group in groups) {
+            //    Console.WriteLine($"#{group.Category}");
+            //    foreach(var book in group.Books) {
+            //        Console.WriteLine($" {book.Title}");
+            //    }
 
+            //}
+            var query = Library.Books
+                .Join(Library.Categories,
+                book => book.CategoryId,
+                category => category.Id,
+                (book, category) => new {
+                    book.Title,
+                    book.PublishedYear,
+                    book.Price,
+                    CategoryName = category.Name
+                })
+                .GroupBy(x => x.CategoryName)
+                .OrderBy(x => x.Key);
+
+            foreach(var group in query) {
+                Console.WriteLine("#{0}", group.Key);
+                foreach(var item in group) {
+                    Console.WriteLine(" " + item.Title);
+                }
             }
             
         }
 
         private static void Exercise1_7() {
+            var categoryId = Library.Categories.Single(c => c.Name == "Development").Id;
+            var groups = Library.Books
+                .Where(b => b.CategoryId == categoryId)
+                .GroupBy(b => b.PublishedYear)
+                .OrderBy(b => b.Key);
 
+            foreach(var group in groups) {
+                Console.WriteLine("#{0}", group.Key);
+                foreach(var book in group) {
+                    Console.WriteLine(" {0}", book.Title);
+                }
+            }
         }
 
         private static void Exercise1_8() {
+            var query = Library.Categories
+                .GroupJoin(Library.Books,
+                c => c.Id,
+                b => b.CategoryId,
+                (c, b) => new {
+                    CategoryName = c.Name,
+                    Count = b.Count()
+                })
+                .Where(x => x.Count >= 4);
+            foreach(var category in query) {
+                Console.WriteLine(category.CategoryName);
+            }
         }
     }
 }
