8d5b58b1aa5da8f4d7a908da96069c66a3c1e279
陽色 頓宮 <32234@ojs.ac.jp>
2022-10-27 15:56:45 +0900

    2022/10/27

--- WPF/CollarChecker/MainWindow.xaml.cs
+++ WPF/CollarChecker/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,6 +21,55 @@ namespace CollarChecker {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+
+            DataContext = GetColorList();
+
         }
+
+        /// <summary>
+        /// すべての色を取得するメソッド
+        /// </summary>
+        /// <returns></returns>
+        private MyColor[] GetColorList() {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            setColor();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            setColor(); //起動時に初期状態の設定値（R:0 G:0 B:0）から色を設定
+        }
+
+        //テキストボックスの値を元に色を設定
+        private void setColor() {
+            var r = byte.Parse(rValue.Text);
+            var g = byte.Parse(gValue.Text);
+            var b = byte.Parse(bValue.Text);
+            colorArea.Background = new SolidColorBrush(Color.FromRgb(r, g, b));
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var mycolor = (MyColor)((ComboBox)sender).SelectedItem;
+
+            rSlider.Value = mycolor.Color.R;
+            gSlider.Value = mycolor.Color.G;
+            bSlider.Value = mycolor.Color.B;
+            setColor();
+        }
+
+        private void stockButton_Click(object sender, RoutedEventArgs e) {
+
+        }
+    }
+
+    /// <summary>
+    /// 色と色名を保持するクラス
+    /// </summary>
+    public class MyColor {
+        public Color Color { get; set; }
+        public string Name { get; set; }
     }
 }
